5c68903adfde4efde24ca5bdf541511bf57f1c14
Utilisateur <utilisateur@iMac-de-orange.local>
2024-03-25 10:49:34 +0000

    component dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Dev from './componnents/Dev'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1>Je suis avec la branch DevK</h1>
+      <Dev />
     </>
   )
 }
---
+++ src/componnents/Dev.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dev = () => {
+    return (
+        <div>
+            <h1> je suis sur la branch DEV </h1>
+        </div>
+    );
+}
+
+export default Dev;
